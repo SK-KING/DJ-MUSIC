f9373f54761704261398082da9e7640d0db3df45
SK- KING <gan49ylf@yahoo.com>
2024-11-14 18:53:53 +0530

    Update config.py
    Signed-off-by: SK- KING <gan49ylf@yahoo.com>

--- config.py
+++ config.py
@@ -9,18 +9,18 @@ from pyrogram import filters
 load_dotenv()
 # ------------------------------------
 # -----------------------------------------------------
-API_ID = int(getenv("API_ID", "16457832"))
-API_HASH = getenv("API_HASH", "3030874d0befdb5d05597deacc3e83ab")
+API_ID = int(getenv("API_ID", "24175365"))
+API_HASH = getenv("API_HASH", "6c671b52c2acbc665e239c006ed890a2")
 # ------------------------------------------------------
-BOT_TOKEN = getenv("BOT_TOKEN", "7638229482:AAHmvpFaVQzInvrGsBMlotrHmpnzQwwPhAk")
+BOT_TOKEN = getenv("BOT_TOKEN", "7317529707:AAFzgy65_HvGlkXyxUpNRd9dj6eobRQJrAg")
 # -------------------------------------------------------
-OWNER_USERNAME = getenv("OWNER_USERNAME","@UTTAM470")
+OWNER_USERNAME = getenv("OWNER_USERNAME","@Cute_girl_ll")
 # --------------------------------------------------------
-BOT_USERNAME = getenv("BOT_USERNAME" , "BABY_MUSIC09_BOT")
+BOT_USERNAME = getenv("BOT_USERNAME" , "MUSIC_SAKSHI_BOT")
 # --------------------------------------------------------
-BOT_NAME = getenv("BOT_NAME" , "╼⃝𖠁 𝐁ʌʙʏ ꭙ 𝐌ᴜsɪᴄ 𖠁⃝╾")
+BOT_NAME = getenv("BOT_NAME" , "🌸𝆺𝅥⃝🦋 ⃪ͥ͢ ᷟ•× 𝐒𝐀𝐊𝐒𝐇𝐈 °‌❤️‍🩹✘ᴅ 🕊")
 # ---------------------------------------------------------
-ASSUSERNAME = getenv("ASSUSERNAME" , "╼⃝𖠁 𝐁ʌʙʏ ꭙ 𝐌ᴜsɪᴄ 𖠁⃝╾")
+ASSUSERNAME = getenv("ASSUSERNAME" , "🌸𝆺𝅥⃝🦋 ⃪ͥ͢ ᷟ•× 𝐒𝐀𝐊𝐒𝐇𝐈 °‌❤️‍🩹✘ᴅ 🕊")
 
 API_KEY = "abc921ff654bcf7b3faff8f775d781d8d27d32bfd02d6692eea30249ba781c8b"  # अपना API key यहाँ डालें
 # ---------------------------------------------------------
@@ -28,7 +28,7 @@ API_KEY = "abc921ff654bcf7b3faff8f775d781d8d27d32bfd02d6692eea30249ba781c8b"  # 
 
 #---------------------------------------------------------------
 #---------------------------------------------------------------
-MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://TEAMBABY01:UTTAMRATHORE09@cluster0.vmjl9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://gefigo2922:gefigo2922@music990.29azj.mongodb.net/?retryWrites=true&w=majority&appName=Music990")
 #---------------------------------------------------------------
 #---------------------------------------------------------------
 
@@ -37,10 +37,10 @@ DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 17000))
 # ----------------------------------------------------------------
 
 # ----------------------------------------------------------------
-LOGGER_ID = int(getenv("LOGGER_ID", "-1002043570167"))
+LOGGER_ID = int(getenv("LOGGER_ID", "-1002188495981"))
 # ----------------------------------------------------------------
 # ----------------------------------------------------------------
-OWNER_ID = int(getenv("OWNER_ID", "7131351568"))
+OWNER_ID = int(getenv("OWNER_ID", "7423400816"))
 # -----------------------------------------------------------------
 # -----------------------------------------------------------------
 # config.py
@@ -56,7 +56,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 # ----------------------------------------------------------------
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/BABY-MUSIC/SPOTIFY_MUSIC",
+    "https://github.com/SK-KING/DJ-MUSIC",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
@@ -70,10 +70,10 @@ GIT_TOKEN = getenv(
 
 # ------------------------------------------------------------------------
 # -------------------------------------------------------------------------
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/BABY09_WORLD")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+OL6jdTL7JAJjYzVl")
-SOURCE = getenv("SOURCE", "https://github.com/BABY-MUSIC/BABYXMUSIC")
-CHAT = getenv("CHAT", "https://t.me/chattinghuboo")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/SAKSHI_UPDATES")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/SAKSHI_SUPPORT")
+SOURCE = getenv("SOURCE", "https://github.com/SK-KING/DJ-MUSIC")
+CHAT = getenv("CHAT", "https://t.me/tg_friends_hub")
 # ------------------------------------------------------------------------------
 # -------------------------------------------------------------------------------
 
@@ -119,7 +119,7 @@ TG_VIDEO_FILESIZE_LIMIT = int(getenv("TG_VIDEO_FILESIZE_LIMIT", "5242880000"))
 # ------------------------------------
 # ------------------------------------
 # ------------------------------------
-STRING1 = getenv("STRING_SESSION", "BQCZzqEAUvJwTOLhpzb9x_GBT9LPTai1q8d_9YKuGw38KKFaxJ0dwJSwaZBEs-nAJeZzpJJmNosjfdaOX3wQUPnFpH0lU0YzKtjPBNTlmb__dNVMO4SpX9quQGSkYYy6WR7At_ZWcJ0slcKRIPqPnDdxw_x5qtgKJTEaC85p0GyiVIE6qf7BBDV96CjeaTC6nhc8_Es0jyBvzDnmqyd4sSS3g4jn9d3VPIiIFoUEdY7PpiTtw050_sz0ArAnDN1Hi-qeLCmPKTjJUcM4LZ8rZwrz1U25aH9msNhGMO5JPBV39b3O02Nl5kgalkfwhjZt9LAg8OzLFAE-DQH9wAAAAGCXgzzAA")
+STRING1 = getenv("STRING_SESSION", "BACZzqEAkjb-fflnfCheX-ZNHgwWnrkHh5QSYVPwmCRdka2GX8XWBp9CDjQacJSVN1VS9w00bJ7XoScBOlhmgKZOdGtsRYEjXt22EDQhCfJrL5icd81tK4C3gLfuaft5N0_25pmHJgyeOArk_Z_eL74RpWE0nTLyx3Ax2KnPepYwwrH5PzaTPJkn8KilIlsOtqmTXz_MTzwKEneGOVLltHcobLePzzTFM5dPKM9hnUNVV-W9zNmVYb2gWWsd-SdrVIihajMxStBSsn_9_pRwmpAvsMkMiYiyp72u45_WbIuYoXvIvLUgvtT-W2Wy5yyYd66wMxmUo8BzkETg9SjuXbaiOa6TWwAAAAHIEVb0AA")
 BANNED_USERS = filters.user()
 adminlist = {}
 lyrical = {}
